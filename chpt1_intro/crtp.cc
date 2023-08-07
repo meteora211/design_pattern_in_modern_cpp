14a7a97f8a214e9d130f01f073d5e753c13bf8cf
zhaoruic <czhaor@gmail.com>
2023-08-07 21:53:33 +0800

    crtp and mixin.

--- chpt1_intro/crtp.cc
+++ chpt1_intro/crtp.cc
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
 template<typename Derived>
 struct Base {
@@ -20,9 +22,34 @@ struct Bar : public Base<Bar> {
   }
 };
 
+template<typename Printable>
+class RepeatPrintable {
+public:
+  void repeat(int n) const {
+    for (auto i = 0; i < n; ++i) {
+      static_cast<const Printable*>(this)->print();
+    }
+  }
+};
+
+class Name : public RepeatPrintable<Name> {
+public:
+  Name(std::string firstName, std::string lastName) :
+    firstName_(std::move(firstName)), lastName_(std::move(lastName)) {}
+  void print() const {
+    std::cout << firstName_ << ", " << lastName_ << std::endl;
+  }
+private:
+  std::string firstName_;
+  std::string lastName_;
+};
+
 int main() {
   Foo f;
   Bar b;
   f.print();
   b.print();
+
+  Name czr("Zhaorui", "Chen");
+  czr.repeat(5);
 }
--- chpt1_intro/mixin.cc
+++ chpt1_intro/mixin.cc
@@ -1,6 +1,40 @@
 #include <iostream>
+#include <string>
 
-template<typename T> 
-struct Mixin : T {
+using namespace std;
 
+template<typename Printable> 
+struct RepeatPrintable : Printable {
+  explicit RepeatPrintable(const Printable& printable) : Printable(printable) {}
+
+  void repeat(int n) {
+    for (auto i = 0; i < n; ++i) {
+      this->print();
+    }
+  }
 };
+
+class Name {
+public:
+  Name(std::string firstName, std::string lastName) :
+    firstName_(std::move(firstName)), lastName_(std::move(lastName)) {}
+
+  void print() const {
+    std::cout << firstName_ << ", " << lastName_ << std::endl;
+  }
+
+private:
+  std::string firstName_;
+  std::string lastName_;
+};
+
+template<typename Printable>
+RepeatPrintable<Printable> repeatPrintable(const Printable& p) {
+  return RepeatPrintable<Printable>(p);
+}
+
+
+int main() {
+  Name czr("Zhaorui", "Chen");
+  repeatPrintable(czr).repeat(5);
+}
