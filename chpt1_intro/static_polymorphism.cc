35d886084f59cb9b41c2fa1fd8b0e563dcd56d8d
zhaorui chen <czhaor@gmail.com>
2023-08-10 17:10:09 +0800

    more examples.

---
+++ chpt1_intro/static_polymorphism.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <string_view>
+
+using namespace std;
+// actually a CRTP
+
+template<typename TImpl>
+class Notifier {
+public:
+  void alertSMS(std::string_view msg) {
+    impl().sendAlertSMS(msg);
+  }
+  void alertEmail(std::string_view msg) {
+    impl().sendAlertEmail(msg);
+  }
+private:
+  TImpl& impl() {
+    return static_cast<TImpl&>(*this);
+  }
+  // friend TImpl;
+};
+
+class TestNotifier: public Notifier<TestNotifier> {
+public:
+  void sendAlertSMS(std::string_view msg) {
+    std::cout << "Test AlertSMS: " << msg << std::endl;
+  }
+  void sendAlertEmail(std::string_view msg) {
+    std::cout << "Test AlertEmail: " << msg << std::endl;
+  }
+};
+
+// XXX: alertAll interface provide an abstract interface.
+// `Notifier<TImpl>` uses CRTP to achieve static polymorphism
+template<typename TImpl>
+void alertAll(Notifier<TImpl>& notifier, std::string_view msg) {
+  notifier.alertSMS(msg);
+  notifier.alertEmail(msg);
+}
+
+int main () {
+  TestNotifier tn;
+  alertAll(tn, "alert msg");
+}
---
+++ chpt1_intro/static_polymorphism_concept.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string_view>
+#include <concepts>
+
+using namespace std;
+
+template<typename TImpl>
+concept IsNotifier = requires(TImpl t) {
+  t.alertSMS(std::string_view{});
+  t.alertEmail(std::string_view{});
+};
+
+class TestNotifier {
+public:
+  void alertSMS(std::string_view msg) {
+    std::cout << "Test AlertSMS: " << msg << std::endl;
+  }
+  void alertEmail(std::string_view msg) {
+    std::cout << "Test AlertEmail: " << msg << std::endl;
+  }
+};
+
+// XXX: alertAll interface provide an abstract interface by concept IsNotifier 
+template<IsNotifier TImpl>
+void alertAll(TImpl& notifier, std::string_view msg) {
+  notifier.alertSMS(msg);
+  notifier.alertEmail(msg);
+}
+
+int main () {
+  TestNotifier tn;
+  alertAll(tn, "alert msg");
+}
